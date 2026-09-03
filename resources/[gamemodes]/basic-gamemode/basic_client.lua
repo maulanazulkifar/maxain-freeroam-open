7c418fcbcee76f199a4004fda357f9a42ce29a9b
maul <maulana.zulkifar@oppo-aed.id>
2026-09-03 14:27:11 +0700

    feat: implement initial spawn configuration and add custom server loading screen

--- resources/[gamemodes]/basic-gamemode/basic_client.lua
+++ resources/[gamemodes]/basic-gamemode/basic_client.lua
@@ -1,4 +1,12 @@
 AddEventHandler('onClientMapStart', function()
+  exports.spawnmanager:addSpawnPoint({
+    x = 195.55,
+    y = -933.36,
+    z = 30.69,
+    heading = 144.0,
+    model = 'a_m_y_skater_01',
+    skipFade = false
+  })
   exports.spawnmanager:setAutoSpawn(true)
   exports.spawnmanager:forceRespawn()
 end)
